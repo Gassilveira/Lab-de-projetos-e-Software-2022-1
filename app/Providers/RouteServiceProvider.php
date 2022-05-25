d5f46c2776da2df8224a10e1b7fde484af180de0
Leonardo Oliveira <leo.oliveira8@gmail.com>
2022-05-24 21:34:44 -0300

    block web routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,12 +29,16 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
+            /*
             Route::prefix('api')
                 ->middleware('api')
                 ->group(base_path('routes/api.php'));
 
+
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group(base_path('routes/web.php'));*/
         });
     }
 
