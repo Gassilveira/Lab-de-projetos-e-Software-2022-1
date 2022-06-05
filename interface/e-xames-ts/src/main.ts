22d3ca6fbdae7ef80fae5ac272d072ec15c05d7d
Leonardo Oliveira <leo.oliveira8@gmail.com>
2022-06-05 10:58:03 -0300

    login and main

--- interface/e-xames-ts/src/main.ts
+++ interface/e-xames-ts/src/main.ts
@@ -4,6 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios';
+import VueAxios from 'vue-axios'
+import VueSocialauth from 'vue-social-auth'
+
 import VueSidebarMenu from "vue-sidebar-menu";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import '@/assets/base.css';
@@ -13,5 +17,14 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueSidebarMenu)
+app.use(VueAxios, axios)
+app.use(VueSocialauth, {
+    providers: {
+      google: {
+        clientId: '',
+        redirectUri: '/auth/github/callback' // Your client app URL
+      }
+    }
+  })
 app.config.productionTip = false;
 app.mount('#app')
