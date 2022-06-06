332ee474f2b64fc0219f25512ceceee8eaadd042
Leonardo Oliveira <leo.oliveira8@gmail.com.br>
2022-06-05 21:32:29 -0300

    remove gauth

--- interface/e-xames-ts/src/main.ts
+++ interface/e-xames-ts/src/main.ts
@@ -1,30 +1,22 @@
+/* vue */
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+const app = createApp(App)
 
-import axios from 'axios';
-import VueAxios from 'vue-axios'
-import VueSocialauth from 'vue-social-auth'
-
+/* side Bar */
 import VueSidebarMenu from "vue-sidebar-menu";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import '@/assets/base.css';
 
-const app = createApp(App)
-
+/* uses*/
 app.use(createPinia())
 app.use(router)
 app.use(VueSidebarMenu)
-app.use(VueAxios, axios)
-app.use(VueSocialauth, {
-    providers: {
-      google: {
-        clientId: '',
-        redirectUri: '/auth/github/callback' // Your client app URL
-      }
-    }
-  })
-app.config.productionTip = false;
+
+
+
+/* mount */
 app.mount('#app')
+//app.config.productionTip = false;
--- interface/e-xames-ts/src/views/Login/Login.vue
+++ interface/e-xames-ts/src/views/Login/Login.vue
@@ -16,11 +16,6 @@
                     placeholder="Senha"
                     class="login_txt"
                 />
-                <button @click="AuthProvider('github')">auth Github</button>
-<button @click="AuthProvider('facebook')">auth Facebook</button>
-<button @click="AuthProvider('google')">auth Google</button>
-<button @click="AuthProvider('twitter')">auth Twitter</button>
-<button @click="AuthProvider('vkontakte')">auth Vkontakte</button>
                 <router-link class="login-button button" :to="{path: '/exames'}"><span class="login-text">Login</span> </router-link>
             </div>
         </div>
